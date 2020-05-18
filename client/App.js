5c6a305b2ccd0b94e823e82a281dd6247a5cf539
Carrie Pascale <cpascale43@gmail.com>
2020-05-18 15:35:43 -0400

    chore: set up initial app grid

--- client/App.js
+++ client/App.js
@@ -2,7 +2,19 @@ import React from "react";
 // import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
-  return <div>Placeholder text</div>;
+  return (
+    <div className="container">
+      <h1>Construction Materials Tracker</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add materials</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View materials</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
