1b5425da1f7fecb464c45eb59b4eb1408eaa7356
Carrie Pascale <cpascale43@gmail.com>
2020-05-20 10:45:06 -0400

    test: remove commented out code from app.spec.js

--- tests/components/app.spec.js
+++ tests/components/app.spec.js
@@ -1,18 +1,3 @@
-// import React from "react";
-// import { expect } from 'chai';
-// import { shallow } from "./enzyme";
-
-// import App from "../../client/App";
-
-// describe("App component", () => {
-//   it("renders without crashing", () => {
-//     const wrapper = shallow(<App />);
-
-//     // Expect the wrapper object to be defined
-//     expect(wrapper.exists()).toBe(true);
-//   });
-// });
-
 import React from "react";
 import { expect } from "chai";
 import Enzyme, { shallow } from "enzyme";
@@ -24,10 +9,6 @@ import App from "../../client/App";
 
 describe("React components", () => {
   describe("App", () => {
-    // Before every `it` spec, we instantiate a new `App` react component.
-    // `Article` comes from the client/components/Article.js file.
-    // This component will receive some data in its `fullArticle` prop.
-    // We store this component in a testable wrapper, `appWrapper`.
 
     let appWrapper;
     beforeEach("Create <App /> wrapper", () => {
